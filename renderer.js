0bb3745b796e44641c1758c314eb9ca6f3965600
ninayanez <nina@theblacksea.cc>
2017-09-27 13:02:45 -0700

    rebuild renderer

--- renderer.js
+++ renderer.js
@@ -16,33 +16,32 @@ const log = document.querySelector('.status span')
 const spinner = document.querySelector('.spinner')
 spinner.style.opacity = 0
 
+console.log('loading!')
+
 document.addEventListener('drop', (e) => { 
   e.preventDefault()
 	if (e.dataTransfer.files.length !== 1) return false
 
-  let file = e.dataTransfer.files[0]
-  const ext = path.extname(file) 
-  const stlPath = file.replace(ext, '.stl') 
+  var file = e.dataTransfer.files[0]
+  const ext = path.extname(file.path) 
+  const stlPath = file.path.replace(ext, '.stl') 
 
   log.innerHTML = 'loading ' + file.path
 
-  getPixels(opts.file, (e, pixels) => { 
+  getPixels(file.path, (e, pixels) => { 
     const w = pixels.shape[0]
     const h = pixels.shape[1]
-    const packagedPixels = {
-      width : w, height: h, data: pixels.data, stlFileName: stlPath
-    }
 
     // dither image!
-    pixels.data = floydDither(pixels).data
-    packagedPixels.data = pixels.data
+    const dither = floydDither(pixels)
 
     // draw preview to html canvas
     const data = new ImageData(
-      new Uint8ClampedArray(pixels.data),
+      new Uint8ClampedArray(dither.data),
         pixels.shape[0],
         pixels.shape[1]
     )
+
     ctx.clearRect(0, 0, cvs.width, cvs.height);
     cvs.width = data.width
     cvs.height = data.height
@@ -54,7 +53,12 @@ document.addEventListener('drop', (e) => {
     log.innerHTML = '|･ω･)ﾉ making 3D plate from image pixels...'
     spinner.style.opacity = 1
 
-    const plate = imageToPlate({conf:conf,file:file.path})
+    const plate = imageToPlate({
+      width : w,
+      height: h,
+      conf: conf,
+      data: dither.data
+    })
 
     // export THREE.js model to file
     exportStl({file: stlPath}, plate, (e,success) => {
